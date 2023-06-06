df3964debebeb57b776c35f6c735203c7607d450
EvelinJoemaa <evelin.joemaa@vikk.ee>
2023-06-06 20:09:42 +0300

    As a developer, I want cypress tests to run automatically upon push to GitHub [#185345151]

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const {defineConfig} = require("cypress");
 
 module.exports = defineConfig({
+    projectId: 'kzi7g7',
     e2e: {
         setupNodeEvents(on, config) {
             // implement node event listeners here
